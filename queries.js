c48d77838e8b842e7731c842239bbf32a552e2f6
aeirea <43287032+aeirea@users.noreply.github.com>
2019-09-16 21:01:23 -0400

    Add files via upload
    Bootcamp 2 Done

--- queries.js
+++ queries.js
@@ -1,39 +1,77 @@
 /* Add all the required libraries*/
 
 /* Connect to your database using mongoose - remember to keep your key secret*/
+var fs = require('fs'),
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+    mongoose.connect(config.db.uri, { useNewUrlParser: true });
 
 /* Fill out these functions using Mongoose queries*/
 //Check out - https://mongoosejs.com/docs/queries.html
 
 var findLibraryWest = function() {
-  /* 
+  var query = {name: "Library West"};
+  Listing.findOne(query, function(err, res)
+  {
+    if (err) throw err;
+    console.log(res);
+  });
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
 };
 var removeCable = function() {
+  var query = {code: "CABL"};
+  Listing.findOneAndDelete(query, function(err, res)
+  {
+    if (err) throw err;
+    console.log(res);
+  });
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
 };
 var updatePhelpsLab = function() {
+  var query = {code: "PHL"};
+  var update = {address: "1953 Museum Rd, Gainesville, FL 32603"};
+  Listing.findOneAndUpdate(query,update, function(err, res)
+  {
+    if (err) throw err;
+    //console.log(res);
+	Listing.findOne(query, function(err, res)
+  {
+    if (err) throw err;
+    console.log(res);
+  });
+  });
   /*
-    Phelps Lab address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-    
+    Phelps Lab address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
+
     Correct Address: 1953 Museum Rd, Gainesville, FL 32603
 
    */
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
+  Listing.find().limit(150).exec(function(err, res)
+  {
+    if (err) throw err;
+	res.forEach(function(x){
+		console.log(x);
+	});
+  });
+  /*
+    Retrieve all listings in the database, and log them to the console.
    */
 };
 
 findLibraryWest();
 removeCable();
-updatePhelpsMemorial();
+updatePhelpsLab();
 retrieveAllListings();
